ff469521bbb5c879da59fa8285225e234f3fabce
wutianc <2439911058@qq.com>
2018-12-07 23:23:29 +0800

    '

---
+++ precache-manifest.0efe668d9abdfe9780d386dd4ff997e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c0269a605c738169790a6580ae84bcb",
+    "url": "/static/media/app.1c0269a6.less"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ebc1a702501f939de5c1",
+    "url": "/static/js/main.ebc1a702.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "ebc1a702501f939de5c1",
+    "url": "/static/css/main.d76fa9a1.chunk.css"
+  },
+  {
+    "revision": "190decd367e0f2f330641313b664521d",
+    "url": "/index.html"
+  }
+];
